f665dbd189853ec24a324f1a20c3d79688dde84c
Jose Luis Represa <joseluis.represa.contractor@bbva.com>
2015-12-01 13:15:17 +0100

    Fixed files order

--- test/main.js
+++ test/main.js
@@ -4,12 +4,14 @@ var assert = require('assert');
 
 var defaultFiles = ['test.js'];
 
-it('it should not prepend the file if flag --failOnError', function (cb) {
+it('it should add the file if flag --failOnError', function (cb) {
     process.argv.push('--failOnError');
     var frameworkFactory = require('../lib/index')['framework:qunit-cli-fail'][1];
     frameworkFactory(defaultFiles);
     
     assert.equal(defaultFiles.length, 2, 'files length must be 2');
+    assert.equal(defaultFiles[0], 'test.js', 'files should not be sorted');
+    assert(defaultFiles[1].pattern.match(/karma-qunit-cli-fail.js/), 'last file must be karma-qunit-cli-fail.js');
 
     cb();
 });
